ee06ed801ebe09b1ca75e54dd7a38c598308c74d
DeadKidts <102147126+DeadKidts@users.noreply.github.com>
2024-05-19 17:59:00 +0530

    Update episode.js

--- js/episode.js
+++ js/episode.js
@@ -7,7 +7,7 @@ const dlapi = "/download/";
 
 // Api Server Manager
 
-const AvailableServers = ["https://api3.armedwarriorts.workers.dev/episode/{id}"];
+const AvailableServers = ["https://api3.armedwarriorts.workers.dev"];
 
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
