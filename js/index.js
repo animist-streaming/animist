ecfffb5642aceb6b578b92afe3a0c1a148866e9e
DeadKidts <102147126+DeadKidts@users.noreply.github.com>
2024-05-19 17:42:40 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Api urls
 
-const ProxyApi = "https://proxy.techzbots1.workers.dev/?u=";
+const ProxyApi = "https://api3.armedwarriorts.workers.dev/search/{query}";
 const IndexApi = "/home";
 const recentapi = "/recent/";
 
